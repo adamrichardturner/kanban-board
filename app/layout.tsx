9a288078075d2a93a5ad5c1a1ecc36571575ba7e
Adam Richard Turner <hello@adamrichardturner.dev>
2025-08-09 11:09:33 +0100

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,37 @@ const plusJakartaSans = Plus_Jakarta_Sans({
   variable: '--font-plus-jakarta-sans',
 });
 
+export const viewport =
+  'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no';
+
 export const metadata: Metadata = {
-  title: 'Kanban Board',
-  description: 'A feature-rich Kanban board for task management',
+  title: 'Kanban Board | Adam Richard Turner',
+  description:
+    'Organize tasks, track progress, and boost productivity with our intuitive Kanban board application. Drag-and-drop functionality with real-time updates.',
+  openGraph: {
+    type: 'website',
+    url: 'https://kanban.adamrichardturner.dev',
+    title: 'Kanban Board | Adam Richard Turner',
+    description:
+      'Organize tasks, track progress, and boost productivity with our intuitive Kanban board application. Drag-and-drop functionality with real-time updates.',
+    images: [
+      {
+        url: 'https://kanban.adamrichardturner.dev/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Kanban Board dashboard preview',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://kanban.adamrichardturner.dev',
+    title: 'Kanban Board | Adam Richard Turner',
+    description:
+      'Organize tasks, track progress, and boost productivity with our intuitive Kanban board application. Drag-and-drop functionality with real-time updates.',
+    images: ['https://kanban.adamrichardturner.dev/og-image.png'],
+    creator: '@devadam88',
+  },
 };
 
 export default function RootLayout({
